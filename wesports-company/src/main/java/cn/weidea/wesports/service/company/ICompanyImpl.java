319f06f81a79bdd847ef4de8b72e4e5796d55d5d
solomonluo <isluoxy@gmail.com>
2020-07-30 22:58:13 +0800

    update

---
+++ wesports-company/src/main/java/cn/weidea/wesports/service/company/ICompanyImpl.java
@@ -0,0 +1,8 @@
+package cn.weidea.wesports.service.company;
+
+import com.alibaba.dubbo.config.annotation.Service;
+
+@Service
+public class ICompanyImpl implements ICompanyFieldService {
+
+}
--- wesports-interface/src/main/java/cn/weidea/wesports/service/admin/CompanyService.java
+++
@@ -1,5 +0,0 @@
-package cn.weidea.wesports.service.admin;
-
-public interface CompanyService {
-
-}
