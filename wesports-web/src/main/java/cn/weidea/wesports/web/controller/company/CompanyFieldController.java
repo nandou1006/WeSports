1a9466c1f1a99d268db27a777b71da168eca6422
solomonluo <isluoxy@gmail.com>
2020-08-01 00:17:23 +0800

    update

--- wesports-web/src/main/java/cn/weidea/wesports/web/controller/company/CompanyFieldController.java
+++ wesports-web/src/main/java/cn/weidea/wesports/web/controller/company/CompanyFieldController.java
@@ -7,16 +7,19 @@ import cn.weidea.wesports.service.company.ICompanyFieldService;
 import cn.weidea.wesports.vo.CompanyFieldVo;
 import cn.weidea.wesports.web.service.impl.common.LoginUtils;
 import com.alibaba.dubbo.config.annotation.Reference;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
-@Controller
+@RestController
 @RequestMapping("/api/companyField")
+@Slf4j
 public class CompanyFieldController {
 
     @Reference(version = "${wesports.service.version}")
@@ -25,20 +28,20 @@ public class CompanyFieldController {
     @Autowired
     private RedisUtils redisUtils;
 
-    @RequestMapping(value = "/create",method = RequestMethod.POST)
-    public CommonResult createField(@RequestParam CompanyFieldVo companyFieldVo, HttpServletRequest httpServletRequest){
-       int userId =  Integer.parseInt(LoginUtils.getUserId(httpServletRequest,redisUtils));
-       CompanyField companyField = new CompanyField();
-       companyField.setCompanyId(userId);
-       companyField.setOccupy(0);
-       companyField.setNumber(companyFieldVo.getNumber());
-       companyField.setType(companyFieldVo.getType());
-       return companyFieldService.createField(companyField);
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public CommonResult createField(@RequestParam CompanyFieldVo companyFieldVo, HttpServletRequest httpServletRequest) {
+        int userId = Integer.parseInt(LoginUtils.getUserId(httpServletRequest, redisUtils));
+        CompanyField companyField = new CompanyField();
+        companyField.setCompanyId(userId);
+        companyField.setOccupy(0);
+        companyField.setNumber(companyFieldVo.getNumber());
+        companyField.setType(companyFieldVo.getType());
+        return companyFieldService.createField(companyField);
     }
 
-    @RequestMapping(value = "/delect",method = RequestMethod.DELETE)
-    public CommonResult delectField(@RequestParam CompanyFieldVo companyFieldVo,HttpServletRequest httpServletRequest){
-        int userId =  Integer.parseInt(LoginUtils.getUserId(httpServletRequest,redisUtils));
+    @RequestMapping(value = "/delect", method = RequestMethod.DELETE)
+    public CommonResult delectField(@RequestParam CompanyFieldVo companyFieldVo, HttpServletRequest httpServletRequest) {
+        int userId = Integer.parseInt(LoginUtils.getUserId(httpServletRequest, redisUtils));
         CompanyField companyField = new CompanyField();
         companyField.setCompanyId(userId);
         companyField.setType(companyFieldVo.getType());
@@ -46,14 +49,17 @@ public class CompanyFieldController {
         return companyFieldService.delectField(companyField);
     }
 
-    @RequestMapping(value = "/selectTypeById",method = RequestMethod.GET)
-    public CommonResult selectFieldTypeById(@RequestParam int id){
+    @RequestMapping(value = "/selectTypeById", method = RequestMethod.GET)
+    public CommonResult selectFieldTypeById(@RequestParam int id) {
         return companyFieldService.selectTypeById(id);
     }
 
-    @RequestMapping(value = "/selectAllField",method = RequestMethod.GET)
-    public CommonResult selectAllField(){
-        return companyFieldService.selectAllField();
+    @RequestMapping(value = "/selectAllField", method = RequestMethod.GET)
+    public CommonResult selectAllField() {
+        log.info("进入selectAllField");
+        CommonResult commonResult = companyFieldService.selectAllField();
+        log.info("{}", commonResult);
+        return commonResult;
     }
 
 
