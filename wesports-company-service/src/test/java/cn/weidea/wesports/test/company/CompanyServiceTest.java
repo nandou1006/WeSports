626db49e9870a666594864e18ced4a2bbde656b4
leeebai <120190695@qq.com>
2020-07-31 21:50:44 +0800

    Merge branch '1.0.dev' of https://github.com/nandou1006/WeSports into 1.0.dev
     Conflicts:
    	wesports-interface/src/main/java/cn/weidea/wesports/service/admin/CompanyService.java

---
+++ wesports-company-service/src/test/java/cn/weidea/wesports/test/company/CompanyServiceTest.java
@@ -0,0 +1,8 @@
+package cn.weidea.wesports.test.company;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
