f94fb71ec65d99b6c18c6a216a44351bc545aab8
leeebai <120190695@qq.com>
2020-07-31 09:33:27 +0800

    Merge branch '1.0.dev' of https://github.com/nandou1006/WeSports into 1.0.dev
     Conflicts:
    	wesports-interface/src/main/java/cn/weidea/wesports/service/admin/CompanyService.java

--- wesports-company/src/main/java/cn/weidea/wesports/service/impl/company/CompanyServiceImpl.java
+++ wesports-company/src/main/java/cn/weidea/wesports/service/impl/company/CompanyServiceImpl.java
@@ -1,6 +1,7 @@
-package cn.weidea.wesports.service;
+package cn.weidea.wesports.service.impl.company;
 
 import cn.weidea.wesports.entity.Company;
+import cn.weidea.wesports.mapper.CompanyMapper;
 import cn.weidea.wesports.service.admin.*;
 import com.alibaba.dubbo.config.annotation.Service;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class CompanyServiceImpl implements CompanyService {
 
     @Autowired
-    public Company company;
+    public CompanyMapper companyMapper;
 
     public int create(String name,String uscc,String password,String profile){
 
--- wesports-company/src/main/java/cn/weidea/wesports/service/impl/company/ICompanyImpl.java
+++
@@ -1,9 +0,0 @@
-package cn.weidea.wesports.service.impl.company;
-
-import cn.weidea.wesports.service.company.ICompanyFieldService;
-import com.alibaba.dubbo.config.annotation.Service;
-
-@Service
-public class ICompanyImpl implements ICompanyFieldService {
-
-}
