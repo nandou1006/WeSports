34249b9e371f3ab5adbe38c568a28c3fce44fff5
qinZss <15527128993@163.com>
2020-08-01 11:54:18 +0800

    引入axios

--- src/main.js
+++ src/main.js
@@ -3,10 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-// import axios from 'axios'
-// import VueAxios from 'vue-axios'
-// Vue.use(VueAxios, axios)
+import axios from 'axios'
+
 Vue.config.productionTip = false
+Vue.prototype.$axios = axios
 
 /* eslint-disable no-new */
 new Vue({
